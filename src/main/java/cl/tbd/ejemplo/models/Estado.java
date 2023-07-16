43024201eb2264090f76e2e82331f3cbf4f05624
Clemente Aguilar <clemente.aguilar@usach.cl>
2023-07-16 18:23:11 -0400

    errores de merge

--- src/main/java/cl/tbd/ejemplo/models/Estado.java
+++ src/main/java/cl/tbd/ejemplo/models/Estado.java
@@ -7,11 +7,7 @@ public class Estado {
     @BsonId
     ObjectId _id;
     String descripcion;
-<<<<<<< HEAD
-    //Tarea tarea;
-=======
 //    Tarea tarea;
->>>>>>> 3b7fce24509ce941220d769db3089dffa6cffc86
 
     public ObjectId get_id() { return this._id; }
     public void set_id(ObjectId _id) { this._id = _id; }
@@ -19,11 +15,7 @@ public class Estado {
     public String getDescripcion() { return this.descripcion; }
     public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
 
-<<<<<<< HEAD
-    //public Tarea getTarea() { return this.tarea; }
-    //public void setTarea(Tarea tarea) { this.tarea = tarea; }
-=======
 //    public Tarea getTarea() { return this.tarea; }
 //    public void setTarea(Tarea tarea) { this.tarea = tarea; }
->>>>>>> 3b7fce24509ce941220d769db3089dffa6cffc86
+
 }
