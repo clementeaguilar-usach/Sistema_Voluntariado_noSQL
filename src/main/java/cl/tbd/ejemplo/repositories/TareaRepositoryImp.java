c39c1e4809bc9e00287d924d8ceea1f0c9150567
Clemente Aguilar <clemente.aguilar@usach.cl>
2023-07-16 18:35:52 -0400

    Me faltó un implements en el repository

--- src/main/java/cl/tbd/ejemplo/repositories/TareaRepositoryImp.java
+++ src/main/java/cl/tbd/ejemplo/repositories/TareaRepositoryImp.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class TareaRepositoryImp {
+public class TareaRepositoryImp implements TareaRepository {
 
     @Autowired
     MongoDatabase database;
